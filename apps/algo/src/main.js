49942c5eb13939ed3efc3d0422a2292b9af4108f
Kanato Shimabukuro <kanato.shimabukuro@gmail.com>
2025-06-28 19:47:33 +0900

    ALGOゲーム 実装中

--- apps/algo/src/main.js
+++ apps/algo/src/main.js
@@ -35,6 +35,46 @@ const app = createApp({
             console.log(`[ ${(new Date()).toISOString()} ] ${d}`);
         };
 
+        const isObject = function(value) {
+            return value !== null && typeof value === 'object';
+        };
+
+        const LS_KEY = location.pathname;
+
+        const storageSetData = (value = {}, key = '') => {
+            const lsKey = LS_KEY ?? location.pathname;
+
+            let setData = key ? Object.assign(storageGetData() ?? {}, { [key]: value }) : value;
+
+            if(!isObject(setData)) return false;
+
+            setData = JSON.stringify(setData);
+
+            localStorage.setItem(lsKey, setData);
+
+            return true;
+        };
+
+        const storageGetData = (key = '') => {
+            const lsKey = LS_KEY ?? location.pathname;
+
+            let lsData = localStorage.getItem(lsKey);
+
+            if(!lsData) return null;
+
+            lsData = JSON.parse(lsData);
+
+            if(!isObject(lsData)) return false;
+
+            if(key) return key in lsData ? lsData[key] : null;
+
+            return lsData;
+        };
+
+        const randomHex = () => {
+            return [...Array(16)].map(()=>"0123456789abcdef"[Math.floor(Math.random()*16)]).join('');
+        };
+
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
         const callAPI = async (uri = '', queries = '', requestBody = null, endpoint = API_ENDPOINTS[0]) => {
@@ -86,8 +126,16 @@ const app = createApp({
 
         const dialog_settings_visible = ref(false);
 
-        const dialog_settings = () => {
+        const dialog_settings = (command = '') => {
             dialog_settings_visible.value = true;
+
+            switch(command) {
+                case 'local-storage-clear': {
+                    if(storageSetData()) snackbar('ローカルストレージをクリアしました', 'success');
+
+                    break;
+                }
+            }
         };
 
         const dialog_select_mode_visible = ref(false);
@@ -128,7 +176,7 @@ const app = createApp({
 
         const game = ref({});
 
-        const game_initialize = async () => {
+        const game_initialize = () => {
             game.value = {
                 mode: '',
                 state: 0,
@@ -140,32 +188,257 @@ const app = createApp({
             };
         };
 
+        const game_update = async () => {
+            for(let i = 0; i < game.value.cards.length; i++) {
+                for(let j = 0; j < game.value.cards[i].length; j++) {
+                    let num = game.value.cards_status[i][j];
+
+                    game.value.cards[i][j] = {
+                        num: Math.ceil(num / 2),
+                        color: (num % 2 === 0) ? 'black' : 'white'
+                    };
+                }
+            }
+
+            if(game.value.mode == 'single_continue') {
+                let session = storageGetData('session');
+
+                try {
+                    const response = await callAPI('', '', { method: 'db-data-set', params: { name: `algo-${session}`, data: game.value } }, API_ENDPOINTS[1]);
+
+                    if(response.data.result != 'success') throw new Error('api-call-failed');
+                } catch(e) {
+                    logging(e);
+
+                    snackbar('APIとの処理でエラーが起きました', 'error');
+                }
+            }
+        };
+
+        const game_create = () => {
+            let size_x = 5;
+            let size_y = 8;
+
+            game.value.cards_size = { x: size_x, y: size_y };
+
+            for(let i = 0; i < size_x; i++) {
+                game.value.cards[i] = [];
+
+                for(let j = 0; j < size_y; j++) {
+                    game.value.cards[i][j] = {
+                        num: 0,
+                        color: 'white'
+                    };
+                }
+            }
+
+            for(let i = 0; i < size_x; i++) {
+                game.value.cards_status[i] = [];
+
+                for(let j = 0; j < size_y; j++) {
+                    game.value.cards_status[i][j] = 0;
+                }
+            }
+
+            let max_card_num = (size_x - 2) * size_y;
+
+            const numbers = Array.from({ length: max_card_num }, (_, i) => i + 1);
+
+            for(let i = numbers.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+
+                [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
+            }
+
+            game.value.cards_queue = numbers;
+
+            for(let j = 0; j < size_y; j++) {
+                game.value.cards_status[Math.floor(size_x / 2)][j] = game.value.cards_queue.shift();
+            }
+        };
+
+        const game_prepare = async () => {
+            if(game.value.mode == 'single_continue') {
+                let session  = storageGetData('session');
+                let response = null;
+
+                try {
+                    response = await callAPI('', '', { method: 'db-data-get', params: { name: `algo-${session}` } }, API_ENDPOINTS[1]);
+
+                    if(response.data.result == 'success') {
+                        game.value = response.data.value;
+                    } else {
+                        game_create();
+                    }
+                } catch(e) {
+                    logging(e);
+
+                    snackbar('APIとの処理でエラーが起きました', 'error');
+                }
+            } else {
+                game_create();
+            }
+
+            game_update();
+        };
+
         const game_start = async (mode = '') => {
+            let session = storageGetData('session');
+
             switch(mode) {
                 case 'single': {
-                    dialog_select_single_start_mode();
-                    return;
+                    if(!session) return game_start('single_new');
+
+                    return dialog_select_single_start_mode();
                 }
 
                 case 'single_new': {
-                    break;
+                    session = randomHex();
+
+                    storageSetData(session, 'session');
+
+                    return game_start('single_continue');
                 }
 
-                case 'single_continue': {
-                    break;
-                }
-
+                case 'single_continue':
                 case 'offline': {
+                    game_initialize();
+
+                    game.value.mode = mode;
+
                     break;
                 }
 
                 default:
-                    break;
+                    return;
             }
 
-            await game_initialize();
+            game_prepare();
 
             container_visible.value = true;
+        };
+
+        const game_over = () => {
+            window.alert('ゲームオーバー');
+
+            if(game.value.mode == 'single_continue') storageSetData(null, 'session');
+
+            location.reload();
+        }
+
+        const game_check = () => {
+            let size_x = game.value.cards_size.x;
+            let size_y = game.value.cards_size.y;
+            let center = Math.floor(size_x / 2);
+
+            for(let j = 0; j < size_y; j++) {
+                let c = game.value.cards_status[center][j];
+
+                let num = 0;
+
+                for(let i = (center - 1); i >= 0; i--) {
+                    if(game.value.cards_status[i][j] == 0) continue;
+
+                    if(!num) {
+                        num = game.value.cards_status[i][j];
+
+                        continue;
+                    }
+
+                    if(c > num && num > game.value.cards_status[i][j]) {
+                        num = game.value.cards_status[i][j];
+                    } else if(c < num && num < game.value.cards_status[i][j]) {
+                        num = game.value.cards_status[i][j];
+                    } else {
+                        game_over();
+
+                        return;
+                    }
+                }
+
+                num = 0;
+
+                for(let i = (center + 1); i < size_x; i++) {
+                    if(game.value.cards_status[i][j] == 0) continue;
+
+                    if(!num) {
+                        num = game.value.cards_status[i][j];
+
+                        continue;
+                    }
+
+                    if(c > num && num > game.value.cards_status[i][j]) {
+                        num = game.value.cards_status[i][j];
+                    } else if(c < num && num < game.value.cards_status[i][j]) {
+                        num = game.value.cards_status[i][j];
+                    } else {
+                        game_over();
+
+                        return;
+                    }
+                }
+            }
+        };
+
+        const game_placing_card = (x = 0, y = 0) => {
+            let size_x = game.value.cards_size.x;
+            let size_y = game.value.cards_size.y;
+            let center = Math.floor(size_x / 2);
+
+            if(center == x) {
+                snackbar('ここには置けません');
+
+                return;
+            }
+
+            let num = 0;
+
+            for(let i = 0; i < game.value.cards.length; i++) {
+                num += game.value.cards_status[i][y] != 0 ? 1 : 0;
+            }
+
+            if(num >= Math.ceil(size_x / 2)) {
+                snackbar('もう置けません');
+
+                return;
+            }
+
+            if(center > x) {
+                for(let i = center; i > x; i--) {
+                    if(game.value.cards_status[i][y] == 0) {
+                        snackbar('間にカードがありません');
+
+                        return;
+                    }
+                }
+            }
+
+            if(center < x) {
+                for(let i = center; i < x; i++) {
+                    if(game.value.cards_status[i][y] == 0) {
+                        snackbar('間にカードがありません');
+
+                        return;
+                    }
+                }
+            }
+
+            if(center > x) {
+                if(game.value.cards_status[x][y] != 0) {
+                    game.value.cards_status[x - 1][y] = game.value.cards_status[x][y];
+                }
+            }
+
+            if(center < x) {
+                if(game.value.cards_status[x][y] != 0) {
+                    game.value.cards_status[x + 1][y] = game.value.cards_status[x][y];
+                }
+            }
+
+            game.value.cards_status[x][y] = game.value.cards_queue.shift();
+
+            game_update();
+            game_check();
         };
 
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -223,6 +496,7 @@ const app = createApp({
             dialog_select_single_start_mode,
 
             game,
+            game_placing_card,
             developer,
 
             container_visible
@@ -284,6 +558,13 @@ const app = createApp({
                         <v-toolbar-title>設定</v-toolbar-title>
                     </v-toolbar>
                     <v-list lines="two">
+                        <v-list-subheader>General</v-list-subheader>
+                        <v-list-item
+                            title="ローカルストレージをクリア"
+                            subtitle="ブラウザに保存されたアプリケーションデータを全て削除します"
+                            @click="dialog_settings('local-storage-clear')"
+                        ></v-list-item>
+                        <v-divider></v-divider>
                         <v-list-subheader>アプリケーション</v-list-subheader>
                         <v-list-item
                             title="バージョン"
@@ -347,10 +628,9 @@ const app = createApp({
             >
                 <v-card
                     prepend-icon="mdi-play"
-                    title="開始モードを選択"
+                    title="前回の続きから始めますか？"
                 >
                     <v-card-text>
-                        続きから始める場合、セッションコードが必要です。
                         <v-radio-group
                             v-model="dialog_select_single_start_mode_radio"
                             :messages="dialog_select_single_start_mode_radio ? 'モードが選択されました' : 'モードを選択してください'"
